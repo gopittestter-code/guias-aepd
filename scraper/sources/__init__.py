b6d3a44e312f16698284c6515c96734ea6fb6ae5
gopittestter-code <gopittestter@gmail.com>
2026-08-15 19:36:45 +0200

    Create __init__.py

---
+++ scraper/sources/__init__.py
@@ -0,0 +1,6 @@
+from .aepd import AEPDScraper
+from .ccn_cert import CCNCertScraper
+from .incibe import IncibeScraper
+from .edps import EDPSscraper
+
+ALL_SCRAPERS = [AEPDScraper, CCNCertScraper, IncibeScraper, EDPSscraper]

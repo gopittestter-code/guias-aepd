16c5dc9b84689b559f594446e6d9085dbc3fdbc3
gopittestter-code <gopittestter@gmail.com>
2026-08-16 19:47:59 +0200

    Update __init__.py

--- scraper/sources/__init__.py
+++ scraper/sources/__init__.py
@@ -1,7 +1,15 @@
 from .aepd import AEPDScraper
 from .aepd_guides import AEPDGuidesScraper
 from .ccn_cert import CCNCertScraper
+from .ccn_cert_guides import CCNCertGuidesScraper
 from .incibe import IncibeScraper
 from .edps import EDPSscraper
 
-ALL_SCRAPERS = [AEPDScraper, AEPDGuidesScraper, CCNCertScraper, IncibeScraper, EDPSscraper]
+ALL_SCRAPERS = [
+    AEPDScraper,
+    AEPDGuidesScraper,
+    CCNCertScraper,
+    CCNCertGuidesScraper,
+    IncibeScraper,
+    EDPSscraper,
+]
